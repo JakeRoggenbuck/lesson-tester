d2b9f4687712a4588866048e4a39b1e5fdd94c62
jakeroggenbuck <jakeroggenbuck2@gmail.com>
2021-08-27 23:37:22 -0700

    Add website and hover

--- src/App.js
+++ src/App.js
@@ -6,10 +6,16 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h1 id="title">Lesson Tester</h1>
-        <h3 id="author">by Jake Roggenbuck</h3>
+        <h3 id="author">
+          by{' '}
+          <a id="jake" href="https://github.com/jakeroggenbuck">
+            Jake Roggenbuck
+          </a>
+        </h3>
 
         <Code code='print("Hello Simba!")' />
       </header>
+      <a href="https://github.com/JakeRoggenbuck/lesson-tester">Source</a>
     </div>
   );
 }
